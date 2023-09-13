0e23e76efa763e218f397b642106332d6c8a91ae
Mohammed Suhail <jrskmohammed@gmail.com>
2023-09-13 22:44:22 +0530

    Add files via upload

---
+++ myrproject/src/main/java/com/dockerproject/myrproject/MyrprojectApplication.java
@@ -0,0 +1,13 @@
+package com.dockerproject.myrproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyrprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyrprojectApplication.class, args);
+	}
+
+}
---
+++ myrproject/src/test/java/com/dockerproject/myrproject/MyrprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dockerproject.myrproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyrprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
